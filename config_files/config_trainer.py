5b31156bc0af4894b1e15f6341e5d61eb12cc1df
Anna Padee <anna.padee@gmail.com>
2019-11-09 14:49:13 +0100

    Trainer mictrostates patterns

--- config_files/config_trainer.py
+++ config_files/config_trainer.py
@@ -3,6 +3,7 @@
 #-------------------------------------------
 DATA_PATH =  r'C:\Users\brainhacker\Desktop\nd_data'
 
+
 EPOCH = []
 
 #-------------------------------------------
@@ -10,4 +11,11 @@ EPOCH = []
 #-------------------------------------------
 TRIGGER_FILE = r'C:\Users\brainhacker\git\NeuroDecode\neurodecode\triggers\triggerdef_16'
 TRIGGER_DEF = ['INIT']
+
+
+
+#-------------------------------------------
+# Results
+#-------------------------------------------
+OUT_MICROSTATES_FILE = r'C:\Users\brainhacker\Desktop\ms_template.txt'
 
